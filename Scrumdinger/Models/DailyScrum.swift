840ec0266af454417e68861a90fdc576d5880d2b
Seoincam <seoin4715@naver.com>
2025-10-05 22:02:29 +0900

    feat: 기본 Model과 list/detail View 추가

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by 박서인 on 10/5/25.
+//
+
+import Foundation
---
+++ Scrumdinger/Preview Content/DailyScrum+Sample.swift
@@ -0,0 +1,8 @@
+//
+//  DailyScrum+Sample.swift
+//  Scrumdinger
+//
+//  Created by 박서인 on 10/5/25.
+//
+
+import Foundation
